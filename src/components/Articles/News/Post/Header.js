b33471e9870bf44f6b4d2561aed3517f93373de4
aismai <abai.ismailov@gmail.com>
2018-07-10 21:25:59 +0600

    add article Header component

--- src/components/Articles/News/Post/Header.js
+++ src/components/Articles/News/Post/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
 
+import TeamInfo from "../../Elements/TeamInfo";
+import PostData from "../../Elements/PostData";
+
 const Header = props => {
-  return <div>Article Header</div>;
+  const teamInfo = team => {
+    return team ? <TeamInfo team={team} /> : null;
+  };
+
+  const postData = (date, author) => <PostData data={{ date, author }} />;
+
+  return (
+    <div>
+      {teamInfo(props.teamData)}
+      {postData(props.date, props.author)}
+    </div>
+  );
 };
 
 export default Header;
