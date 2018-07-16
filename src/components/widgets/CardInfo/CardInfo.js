9e9d0003edb3672bddffe5a7ad85bf1e4b37b683
aismai <abai.ismailov@gmail.com>
2018-07-16 19:45:21 +0600

    add firebase to NewsList component

--- src/components/widgets/CardInfo/CardInfo.js
+++ src/components/widgets/CardInfo/CardInfo.js
@@ -6,7 +6,7 @@ import styles from "./CardInfo.css";
 const CardInfo = props => {
   const teamName = (teams, team) => {
     let data = teams.find(item => {
-      return item.id === team;
+      return item.teamId === team;
     });
 
     if (data) return data.name;
