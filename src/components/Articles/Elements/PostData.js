119f799ad8bccd4fb08bc9e4005445e7313c62a0
aismai <abai.ismailov@gmail.com>
2018-07-21 19:47:43 +0600

    add moment to PostData

--- src/components/Articles/Elements/PostData.js
+++ src/components/Articles/Elements/PostData.js
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "../Articles.css";
+import moment from "moment";
 
+const formaDate = date => {
+  return moment(date).format(" MM-DD-YYYY");
+};
 const PostData = props => (
   <div className={styles.articlePostData}>
     <div>
-      Date: <span>{props.data.date}</span>
+      Date: <span>{formaDate(props.data.date)}</span>
     </div>
     <div>
       Author: <span>{props.data.author}</span>
