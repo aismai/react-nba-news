d013e7489ef222070b71877e2a55d29961325595
aismai <abai.ismailov@gmail.com>
2018-07-24 19:56:42 +0600

    update state for SignIn component

--- src/components/signin/SignIn.js
+++ src/components/signin/SignIn.js
@@ -2,7 +2,44 @@ import React, { Component } from "react";
 import styles from "./SignIn.css";
 
 class SignIn extends Component {
-  state = {};
+  state = {
+    registerError: "",
+    loading: false,
+    formData: {
+      email: {
+        element: "input",
+        value: "",
+        config: {
+          name: "email_input",
+          type: "email",
+          placehodler: "Enter your email"
+        },
+        validation: {
+          required: true,
+          email: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      },
+      password: {
+        element: "input",
+        value: "",
+        config: {
+          name: "password_input",
+          type: "password",
+          placehodler: "Enter your password"
+        },
+        validation: {
+          required: true,
+          password: true
+        },
+        valid: false,
+        touched: false,
+        validationMessage: ""
+      }
+    }
+  };
 
   render() {
     return <div>SIGN IN</div>;
